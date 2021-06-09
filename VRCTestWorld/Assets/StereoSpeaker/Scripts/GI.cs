3aac534a274a8ae396ad6c46ead1728b201c8852
shivaduke28 <shivaduke28>
2021-06-09 21:22:41 +0900

    simple 3d audio room

---
+++ VRCTestWorld/Assets/StereoSpeaker/Scripts/GI.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace StereoSpeaker
+{
+    public class GI : UdonSharpBehaviour
+    {
+        [SerializeField] private Renderer giRenderer;
+
+        private void Update()
+        {
+            RendererExtensions.UpdateGIMaterials(giRenderer);
+        }
+    }
+}
